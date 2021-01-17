65fe8e74175d92b632aa59fbc8a55ba50854fb6b
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-01-17 13:23:18 +0300

    Remove button that shouldn't had appeared

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -2,7 +2,6 @@
   import { onMount, afterUpdate } from "svelte";
   import Todo from "./Todo.svelte";
   import AddTodoForm from "./AddTodoForm.svelte";
-  import Button from "./Button.svelte";
 
   let todos = [];
 
@@ -56,7 +55,6 @@
 
 <header>
   <h1 class="title">Svelte To-Do</h1>
-  <Button on:click={() => console.log("clicked")} />
 </header>
 <main>
   <div class="inner-block">
